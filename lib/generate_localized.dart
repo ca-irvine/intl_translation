05b8f924e776fc9e8f0cf875d41c66e4dd0e3a65
ryoya ito <110ryoya@gmail.com>
2020-11-16 17:11:03 +0900

    Add declared type to _messagesExistFor catch statement to avoid avoid_catches_without_on_clauses rule

--- lib/generate_localized.dart
+++ lib/generate_localized.dart
@@ -282,7 +282,7 @@ Future<bool> initializeMessages(String localeName) async {
 bool _messagesExistFor(String locale) {
   try {
     return _findExact(locale) != null;
-  } catch (e) {
+  } on Exception catch (_) {
     return false;
   }
 }
